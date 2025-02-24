00a8267c4c8b1eeff63b7c7c18b0ef853d9b22ee
Snehasish321 <snehasishmondal01@gmail.com>
2025-02-24 15:58:43 +0530

    LogInformCreate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 
         {/* Profile Dropdown */}
         <div className="group relative">
-          <img className="w-5 cursor-pointer" src={assets.profile_icon} alt="Profile" />
+          <Link to='/login'><img className="w-5 cursor-pointer" src={assets.profile_icon} alt="Profile" /></Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-custom-cream text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
